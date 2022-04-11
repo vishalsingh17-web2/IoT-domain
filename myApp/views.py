4192271cf4a0e2485abbd9d76264cff37344d2cd
vishalsingh17-web2 <vishal.workcontact@gmail.com>
2022-04-11 18:23:47 +0530

    rnn

--- myApp/views.py
+++ myApp/views.py
@@ -2,20 +2,28 @@ from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 import joblib
+import pickle
+
 
 
 def index(request):
-    clf = joblib.load('myApp/final.sav')
-    encoder = joblib.load('myApp/encoder.sav')
-    return JsonResponse({"Happy family": 'Aman, Akshuni & Akshunn'})
+    try:
+        with open('myApp/rnn_fall.pkl', 'rb') as f:
+            rnn_fall = pickle.load(f)
+        encoder = joblib.load('myApp/rnn_decoder.sav')
+        # clf.predict([[1,2,3,4,5,6,7,8,9]])
+        return JsonResponse({"status": 'Success'})
+    except:
+        return JsonResponse({'error': 'Model not found'})
+    
 
 @csrf_exempt
 def sensorsData(request):
     if request.method == 'POST': 
         received_json_data=json.loads(request.body)
         #Machine learning
-        clf = joblib.load('myApp/final.sav')
-        encoder = joblib.load('myApp/encoder.sav')
+        clf = joblib.load('myApp/rnn_fall.sav')
+        encoder = joblib.load('myApp/rnn_decoder.sav')
         out = clf.predict(received_json_data.get('data'))
         activity = encoder.inverse_transform(out)
         print(activity)
